98ce944501debd73561fe9bf0838c1b09e8f251b
SmitsG <smitsg@ziggo.nl>
2018-05-26 19:24:40 +0200

    Added general flow Added Snakefile Added data and more

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Workflows-bi11
+# Skeleton for the Snakemake tutorial
+
+This repository hosts the skeleton code needed for the [Snakemake tutorial](http://snakemake.readthedocs.io/en/latest/tutorial/welcome.html).
---
+++ data_analysis.py
@@ -0,0 +1,15 @@
+def main()
+    read_file()
+    convert_data()
+    generate_report()
+
+def read_file():
+    print("readfile")
+
+def convert_data():
+    print("convert_data")
+
+def generate_report():
+
+
+main()
