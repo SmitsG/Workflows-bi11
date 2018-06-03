000f8eb2802a8bba98a7a9b7e9cf5a3ab6833708
SmitsG <smitsg@ziggo.nl>
2018-06-03 15:45:58 +0200

    get_gene_ids

--- data_analysis.py
+++ data_analysis.py
@@ -4,25 +4,23 @@ import os
 import re
 INFO = 'Creates a report with gene information'
 
+
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     filename = "gene_ids.txt"
-    gene_id_file = get_gene_ids("RNA-Seq-counts.txt", dir_path + "/" + filename)
+    gene_id_file_location = get_gene_ids("RNA-Seq-counts.txt", dir_path + "/" + filename)
 
 
 def get_gene_ids(input_file, file_location):
-    filename = ntpath.basename(file_location)
     with open(input_file, 'r') as file:
-        line = file.readlines()
-        gene_id_list = (re.split(r'\t+', str(line[1]).strip()))
-        gene_id_list.remove("ID")
-        gene_ids_txt = open(filename, 'w')
-        gene_ids = []
-        for gene_id in gene_id_list:
-            gene_id = gene_id.split(".", 1)[0]
-            if gene_id not in gene_ids:
-                gene_ids.append(gene_id)
-        gene_ids_txt.write(str(gene_ids))
+        data = [i.strip('\n').split('\t') for i in open(input_file)]
+        gene_id_list = []
+        for i in data:
+            i = i[0]
+            if i.startswith("lp"):
+                gene_id_list.append(i)
+        file = open(file_location, "w")
+        file.write(str(gene_id_list))
     return file_location
 
 
