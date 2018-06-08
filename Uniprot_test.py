1d2b76b026dd4a68cb3d0de4890de680a8685d54
Silleke <SCF.vanSambeek@student.han.nl>
2018-06-08 19:10:27 +0200

    Added functionality for more IDs at once

--- Uniprot_test.py
+++ Uniprot_test.py
@@ -1,10 +1,15 @@
 from bioservices.uniprot import UniProt
+
 u = UniProt(verbose=False)
-#print(u.mapping(fr="ACC+ID", to="KEGG_ID", query='P43403'))
+# print(u.mapping(fr="ACC+ID", to="KEGG_ID", query='P43403'))
+# info = u.search("lp_0001", frmt="txt")
+# info = u.search("lp_0001", columns="id")
 #info = u.search("lp_0001", frmt="txt")
-info = u.search("lp_0001", columns="id")
+#print(type(info))
+
+entries = ["lp_001", "lp_0002", "lp_0004", "lp_0005"]
+
+#Krijg veel te veel resultaten. Kan je even kijken? Bron: http://bioservices.readthedocs.io/en/master/references.html#bioservices.uniprot.UniProt
+info = u.get_df(entries, nChunk=100, organism="Lactobacillus plantarum")
 print(info)
-
-
-
 
