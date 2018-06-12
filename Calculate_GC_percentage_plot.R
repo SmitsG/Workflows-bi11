1028e7e74bded9ae13c8cd4f6ecc7764dff5508e
Silleke <SCF.vanSambeek@student.han.nl>
2018-06-12 11:55:21 +0200

    Fixed bug value error and organised the code

--- Calculate_GC_percentage_plot.R
+++ Calculate_GC_percentage_plot.R
@@ -1,4 +1,3 @@
 library("Biostrings")
-fasta_file = readDNAStringSet(filepath = file.choose(), "fasta")
-gc_percentages = letterFrequency(fasta_file, as.prob = T, letters = c("GC"))
-plot(gc_percentages)
+indices = fasta.index(file.choose())
+plot(indices$recno, indices$seqlength)
