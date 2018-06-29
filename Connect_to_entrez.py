b694d5b3720d48eb8dcf83a430d30843b87ab7e7
Silleke <SCF.vanSambeek@student.han.nl>
2018-06-29 15:17:14 +0200

    Removed BLAST functionality and wrote seperate script for it.

--- Connect_to_entrez.py
+++ Connect_to_entrez.py
@@ -1,5 +1,5 @@
 from Bio import Entrez, Medline
-from Bio.Blast import NCBIWWW, NCBIXML
+
 
 
 def main():
@@ -43,25 +43,7 @@ def get_sequence_and_write_to_fasta(records_list):
     genes_fasta.close()
 
 
-def blast_controller(ids_protein):
-    blast_results = []
-    for item in ids_protein:
-        handle_blast = perform_blast(program="blastp", database="nr", sequence=item)
-        blast_result = parse_blast_record(handle_blast)
-        blast_results.append(blast_result)
-    return blast_results
 
-
-def perform_blast(program, database, sequence):
-    handle = NCBIWWW.qblast(program=program, database=database, sequence=sequence)
-    handle.close()
-    return handle
-
-
-def parse_blast_record(handle):
-    results = NCBIXML.parse(handle)
-    results.close()
-    return results
 
 
 def parseMedlineRecords(idlist):
