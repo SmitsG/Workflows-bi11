2dfe0e09db729a8d190298e77d5a02612230429a
Silleke <SCF.vanSambeek@student.han.nl>
2018-06-29 15:29:23 +0200

    Switched PubMed functionality to seperate script.

--- Connect_to_entrez.py
+++ Connect_to_entrez.py
@@ -1,15 +1,11 @@
 from Bio import Entrez, Medline
 
 
-
 def main():
     email = "silleke_frits@hotmail.com"
     ids_protein = search_entrez(email=email, db="protein", retmax=5, term="RAS", rettype="fasta")
-    ids_pubmed = search_entrez(email=email, db="pubmed", retmax=10, term="RAS MAPK pathway", rettype="fasta")
     records_list_protein = fetch_results(email=email, db="protein", ids=ids_protein, rettype="fasta", retmode="xml")
-    records_list_pubmed = parseMedlineRecords(ids_pubmed)
     get_sequence_and_write_to_fasta(records_list_protein)
-    blast_results = blast_controller(ids_protein)
 
 
 def search_entrez(email, db, retmax, term, rettype):
@@ -41,16 +37,5 @@ def get_sequence_and_write_to_fasta(records_list):
         genes_fasta.write(sequence)
         genes_fasta.write("\n")
     genes_fasta.close()
-
-
-
-
-
-def parseMedlineRecords(idlist):
-    handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text")
-    records = Medline.parse(handle)
-    records = list(records)
-    return (records)
-
 
 main()
