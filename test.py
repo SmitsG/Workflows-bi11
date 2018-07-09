a55a2b6a9e3e879110e0ba6a0a4255d8d0911294
SmitsG <smitsg@ziggo.nl>
2018-07-09 23:12:16 +0200

    removed

--- test.py
+++
@@ -1,9 +0,0 @@
-from Bio.KEGG import Enzyme
-from Bio.KEGG import REST
-from bioservices.kegg import KEGG
-
-request = REST.kegg_get("ec:5.4.2.2")
-open("ec_5.4.2.2.txt", "w").write(request.read())
-records = Enzyme.parse(open("ec_5.4.2.2.txt"))
-record = list(records)[0]
-print(record.classname)
