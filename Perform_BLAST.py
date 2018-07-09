3fabe4c06da0acd7f1a44316cb0140392bc34684
Marij van Sambeek <marijvansambeek@iMac-van-Marij.local>
2018-07-09 16:17:16 +0200

    Added file write for snakemake compatibility.

--- Perform_BLAST.py
+++ Perform_BLAST.py
@@ -2,30 +2,28 @@ from Bio.Blast import NCBIWWW, NCBIXML
 
 
 def main():
-    blast_results = blast_controller()
+    blast_handle_list = blast_controller(
+        ids_protein=["MPRHYHYQPSRELHVVVLGAAQFVHNEWIESYDPTIEDSYRTQLQVDGRQVILEILDTAGTEQFVAMRDLYMKTGQGFL",
+                     "MANDEYDFLFKVVLIGDSGVGKSNLLSRFTRNEFNLDSKSTIGVEFATRSIQVDSKTIKAQIWDTAGQERYRAITSAYY"])
+    #write_blast_output_to_xml(blast_handle_list)
 
 
 # It is not possible to BLAST multiple sequences at once, so a for loop is needed
 def blast_controller(ids_protein):
-    blast_results = []
+    blast_handle_list = []
     for item in ids_protein:
         handle_blast = perform_blast(program="blastp", database="nr", sequence=item)
-        blast_result = parse_blast_record(handle_blast)
-        blast_results.append(blast_result)
-    return blast_results
+        blast_handle_list.append(handle_blast)
+    return blast_handle_list
 
 
 # Called from blast_controller
 def perform_blast(program, database, sequence):
     handle = NCBIWWW.qblast(program=program, database=database, sequence=sequence)
+    with open("blast_output.xml", "a") as out_handle:
+        out_handle.write(handle.read())
+        out_handle.close()
     handle.close()
     return handle
-
-
-# Called from blast_controller
-def parse_blast_record(handle):
-    results = NCBIXML.parse(handle)
-    results.close()
-    return results
 
 main()
