da00dd09f6d14a3e2450aa85dcc3aa8887c1c7eb
SmitsG <smitsg@ziggo.nl>
2018-07-09 19:18:57 +0200

    add comments and p53

--- uniprot.py
+++ uniprot.py
@@ -5,14 +5,23 @@ from bioservices import *
 
 def main():
     args = parse_args()
-    uniprot(args.id, args.query)
+    get_uniprot_annotation(args.id, args.query)
 
 
-def uniprot(id, query):
+def get_uniprot_annotation(id, query):
+    """
+    function that gets some uniprot information
+    :param id: uniprot id
+    :param query: uniprot query
+    :return:
+    """
     u = UniProt()
+    # get the fasta of the id.
     print(u.get_fasta(id))
+    # get query results
     print(u.search(query, limit=5))
     df = u.get_df(id)
+    # print Entry ids
     print(df)
 
 
@@ -22,7 +31,7 @@ INFO = 'Uniprot'
 def parse_args():
     """
     Argument parser
-    :return: args: All arguments are parsed to the args. The arguments will be presented in CamelCase.
+    :return: args: All arguments are parsed to the args.
     """
 
     parser = argparse.ArgumentParser(description=INFO,
@@ -31,13 +40,13 @@ def parse_args():
     parser.add_argument("--id",
                         type=str,
                         required=False,
-                        default="P43403",
+                        default="P04637",
                         help="uniprot id")
 
     parser.add_argument("--query",
                         type=str,
                         required=False,
-                        default="ZAP70 and taxonomy:human",
+                        default="P53 and taxonomy:human",
                         help="uniprot query")
 
     # parse all arguments
